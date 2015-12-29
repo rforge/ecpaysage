2cba58bb6bdd98394dc0c8d609ce89329348717f
jprossi <jprossi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-12-29 13:29:55 +0000

    t
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecpaysage@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/zzz.r
@@ -0,0 +1,14 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage("Vous avez chargé la version 2.5.5 du package ECPaysage. Bravo ! \n")
+}
+
+# .onAttach <- function(libname, pkgname) {
+# 
+#   setHook(packageEvent("plyr", "attach"), function(...) {
+#     packageStartupMessage(rule())
+#     packageStartupMessage("You have loaded plyr after dplyr - this is likely ",
+#       "to cause problems.\nIf you need functions from both plyr and dplyr, ",
+#       "please load plyr first, then dplyr:\nlibrary(plyr); library(dplyr)")
+#     packageStartupMessage(rule())
+#   })
+# }
